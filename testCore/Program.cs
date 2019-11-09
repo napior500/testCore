890d1d05369e93279264068da3a95ade79f8831b
Maciej Napiórkowski <maciej500@gmail.com>
2019-11-09 15:26:44 +0100

    Add project files.

---
+++ testCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
