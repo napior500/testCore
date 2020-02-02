ce3e413308b2eff95c2a0af63eb3b4bd3ef9d815
Maciek <maciej500@gmail.com>
2020-02-02 14:14:01 +0100

    adding dockerFile

--- testCore/Program.cs
+++ testCore/Program.cs
@@ -6,8 +6,15 @@ namespace testCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            var counter = 0;
+            var max = args.Length != 0 ? Convert.ToInt32(args[0]) : -1;
+
+            while(max == -1 || counter < max){
+                counter++;
+                Console.WriteLine($"Counter: {counter}");
+                System.Threading.Tasks.Task.Delay(1000).Wait();
+            }
+
         }
     }
 }
